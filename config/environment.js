9c96b824b2f83cd911ccc27863b9ab11a223f3e9
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-08 09:24:03 -0400

    installed: - ember install ember-cli-simple-auth
    -ember install ember-simple-auth-parse
    
    -ember install ember-parse-adapter
    
    and added in the keys for parse. in enviroment

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,18 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      applicationId: 'QVo3ALQG1RDvtcyhflvJvI47C2LyWBF8Zs6lsOvx',
+     restApiId: 'v0kvKnmI0ucQklBvgF1FHqCBEVI9O3UXxhbLsxVd'
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' api.parse.com",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
     }
   };
 
