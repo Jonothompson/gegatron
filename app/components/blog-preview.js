c39cf75925cf3aaea2476925824dbdec728dba7b
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-13 10:40:18 -0400

    Have destroy of blogs working.

--- app/components/blog-preview.js
+++ app/components/blog-preview.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	actions: {
-		destroy: function(blog){
-			this.sendAction('action', blog);
+		destroy: function(){
+			this.sendAction('action', this.get('blog'));
 		}
 	}
 });
--- app/routes/blog/home.js
+++ app/routes/blog/home.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.findAll('blog-post');
+	},
+	
+	actions: {
+		destroy: function(blog){
+			console.log(blog);
+			blog.destroyRecord();
+		},
+		update: function(blog){
+			blog.update();
+			this.transitionTo('blog.show')
+		}
 	}
 });
