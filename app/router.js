6ffbd67356458771a47d4545840c1acf0eca16c3
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-19 13:00:36 -0400

    Sumo paint is embded but not working as it says it should. I have submitted a service ticket to see if any of this can be over come.

--- app/router.js
+++ app/router.js
@@ -37,6 +37,7 @@ Router.map(function() {
     this.route('current');
     this.route('create');
   });
+  this.route('sumo-paint');
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -60,7 +60,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/gegatron/';
   }
 
   return ENV;
