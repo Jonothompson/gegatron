e454499cdaab6ebbf9018446fa339c0e681d866a
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-19 17:41:17 -0400

    Almost have the image upload working, but unfortunarly I am getting a decerialize.get is not a function error.

--- app/routes/pictures/picture-create.js
+++ app/routes/pictures/picture-create.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return this.store.createRecord('sumo-paint');
+	},
+	
+	actions: {
+		save: function(sumo){
+			sumo.save();
+			this.transitionTo('users.current');
+		}
+	}
 });
