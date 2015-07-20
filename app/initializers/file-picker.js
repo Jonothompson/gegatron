cbf4bc529740a07f1d1c79d461fd510cd84c33dd
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-19 20:25:03 -0400

    Trying to add File Picker to sidestep Parse's bad handeling of File uploading

---
+++ app/initializers/file-picker.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data'
+export function initialize() {
+
+}
+
+export default {
+  name: 'file-picker',
+  initialize: initialize
+};
+
+    // <script type="text/javascript" src="//api.filepicker.io/v2/filepicker.js"></script>
+
+    // <input type="filepicker" data-fp-apikey="A2SmW2AexQyeShOQls9Zlz"
+    // onchange="alert(event.fpfile.url)">s
