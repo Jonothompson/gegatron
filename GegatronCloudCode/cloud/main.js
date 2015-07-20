d7e374b6ac5fa03374f7d2c5b35baa0d5f37c570
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-20 15:50:26 -0400

    mobil styling for the about page and header done.

--- GegatronCloudCode/cloud/main.js
+++ GegatronCloudCode/cloud/main.js
@@ -1,6 +1,17 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(receiveSumoPaint, response) {
+Parse.Cloud.define("receiveSumoPaint", function(request, response) {
+  var base64 = "V29ya2luZyBhdCBQYXJzZSBpcyBncmVhdCE=";
+  var file = new Parse.File("sumo-photo.png", { base64: base64 });
+  
+  var fileUploadControl = $("")[0];
+  if (fileUploadControl.files.length > 0) {
+      var file = fileUploadControl.files[0];
+      var name = "sumo-photo.png";
+
+  var parseFile = new Parse.File(name, file);
+  }
+  
   response.success("Hello world!");
 });
