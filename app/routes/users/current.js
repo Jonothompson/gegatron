10029cc3aa488768ae678849e4550f584b75b5f9
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-07-21 20:25:15 -0400

    trying to figure out why the link-to in story board image preview wont work.

--- app/routes/users/current.js
+++ app/routes/users/current.js
@@ -6,30 +6,29 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model: function() {
 		return Ember.RSVP.hash({
 			user: this.get('session.currentUser'),
-			image: this.store.findAll('story-board-image')
+			image: this.store.findQuery('story-board-image', {
+				where: {
+					owner: {
+						__type: "Pointer",
+						className: "_User",
+						objectId: this.get('session.currentUser.id')
+					}
+				}
+			})
 		})
-		// return this.get ('session.currentUser');
-		// return this.store.findAll('story-board-image');
 	},
 	
 	actions: {
 		invalidateSession: function() {
-		this.get('session').invalidate();
+			this.get('session').invalidate();
 		},
+
 		
-	// filePick: function(blob){
-    //  	var username = this.get('session.currentUser.firstName');
-    //   	var upload = this.store.createRecord('story-board-image', {
-    //     	url : blob.url,
-    //     	filename : blob.filename
-    //   	});
-    //   	upload.save(blob);
-    // }
-		
-	filePick: function(params) {
-		params.owner=this.get('session.currentUser');
-		var upload = this.store.createRecord('story-board-image', params);
-		upload.save(params);
+		filePick: function(params) {
+			params.owner = this.get('session.currentUser');
+			var upload = this.store.createRecord('story-board-image', params);
+			upload.save(params);
+			this.modelFor(this.routeName).image.addObject(params);
 		}
 	}
 });
